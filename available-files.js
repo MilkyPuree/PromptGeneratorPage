8ff3ce0aee4e9d5ac8737f6528f954424ce67b11
MilkyPuree <milky.puree@gmail.com>
2025-08-01 04:37:57 +0900

    更新

--- available-files.js
+++ available-files.js
@@ -4,8 +4,14 @@ window.availableFiles = {
     {
       "name": "PromptGenerator.zip",
       "url": "downloads/PromptGenerator.zip",
-      "size": 753045,
-      "lastModified": "2025-08-01T02:23:52.470746"
+      "size": 753975,
+      "lastModified": "2025-08-01T04:36:49.256522"
+    },
+    {
+      "name": "PromptGenerator_v1.0.0_20250801_043649.zip",
+      "url": "downloads/PromptGenerator_v1.0.0_20250801_043649.zip",
+      "size": 753975,
+      "lastModified": "2025-08-01T04:36:49.256522"
     },
     {
       "name": "PromptGenerator_v1.0.0_20250801_022352.zip",
@@ -20,6 +26,6 @@ window.availableFiles = {
       "lastModified": "2025-07-31T22:25:54.870310"
     }
   ],
-  "updated": "2025-08-01T02:23:52.478196",
-  "count": 3
+  "updated": "2025-08-01T04:36:49.261563",
+  "count": 4
 };
