5824aebddfef1aa7b2ecbc5232ef89c87a4fc170
MilkyPuree <milky.puree@gmail.com>
2026-05-17 00:48:34 +0900

    Revert "配布ファイル更新 v1.0.0 - 自動配布システム実行"
    This reverts commit 3f7e1cf77a29bd9eb6c4692d6cf7ed9e653446fa.

--- available-files.js
+++ available-files.js
@@ -4,14 +4,8 @@ window.availableFiles = {
     {
       "name": "PromptGenerator.zip",
       "url": "downloads/PromptGenerator.zip",
-      "size": 650637,
-      "lastModified": "2026-05-16T23:40:59.944336"
-    },
-    {
-      "name": "PromptGenerator_v1.0.0_20260516_234059.zip",
-      "url": "downloads/PromptGenerator_v1.0.0_20260516_234059.zip",
-      "size": 650637,
-      "lastModified": "2026-05-16T23:40:59.944336"
+      "size": 648134,
+      "lastModified": "2026-05-05T13:03:22.434103"
     },
     {
       "name": "PromptGenerator_v1.0.0_20260505_130322.zip",
@@ -68,6 +62,6 @@ window.availableFiles = {
       "lastModified": "2025-08-01T09:49:47.808720"
     }
   ],
-  "updated": "2026-05-16T23:40:59.947339",
-  "count": 11
+  "updated": "2026-05-05T13:03:22.438103",
+  "count": 10
 };
